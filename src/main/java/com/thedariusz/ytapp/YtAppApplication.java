25b9dfa97e494ddcbcd39a01581791198ce9d089
thedariusz <thedariusz@gmail.com>
2022-02-08 20:31:20 +0100

    initial commit

---
+++ src/main/java/com/thedariusz/ytapp/YtAppApplication.java
@@ -0,0 +1,13 @@
+package com.thedariusz.ytapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YtAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thedariusz/ytapp/YtAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thedariusz.ytapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YtAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
