7616c34bdb631b0824fe659cde71bd8c3e5b217a
Dariusz Szczepański <thedariusz@gmail.com>
2022-02-11 21:07:00 +0100

    tried to get jwt token

--- src/main/java/com/thedariusz/ytapp/controllers/HomeController.java
+++ src/main/java/com/thedariusz/ytapp/controllers/HomeController.java
@@ -1,10 +1,27 @@
 package com.thedariusz.ytapp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.oidc.OidcIdToken;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+    final
+    OAuth2AuthorizedClientService oAuth2AuthorizedClientService;
+
+    public HomeController(OAuth2AuthorizedClientService oAuth2AuthorizedClientService) {
+        this.oAuth2AuthorizedClientService = oAuth2AuthorizedClientService;
+    }
 
     @GetMapping("/home")
     public String displayHomePage() {
@@ -12,7 +29,15 @@ public class HomeController {
     }
 
     @GetMapping("/home/yt")
-    public String displayLoginPage() {
+    public String displaySecuredPage(Model model, @AuthenticationPrincipal OidcUser principal) {
+
+
+        System.out.println("Principal =" + principal);
+
+        OidcIdToken idToken = principal.getIdToken();
+        String tokenValue = idToken.getTokenValue();
+
+        System.out.println("Token value = " + tokenValue);
         return "yt";
     }
 }
