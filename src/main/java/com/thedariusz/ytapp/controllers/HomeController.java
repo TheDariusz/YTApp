10c6aca5051524b9a737e0c20c98a5b5d6b7db41
Dariusz Szczepański <thedariusz@gmail.com>
2022-02-19 21:58:02 +0100

    list of yt videos on the page added beginning of user info gathering

--- src/main/java/com/thedariusz/ytapp/controllers/HomeController.java
+++ src/main/java/com/thedariusz/ytapp/controllers/HomeController.java
@@ -2,9 +2,9 @@ package com.thedariusz.ytapp.controllers;
 
 import com.thedariusz.ytapp.model.YtDtoWrapper;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,11 +29,16 @@ public class HomeController {
     }
 
     @GetMapping("/home/yt")
-    public String displaySecuredPage(Model model, @AuthenticationPrincipal OidcUser principal) {
+    public String displaySecuredPage(Model model, OAuth2AuthenticationToken authentication) {
+
+        OAuth2AuthorizedClient client = oAuth2AuthorizedClientService.loadAuthorizedClient(
+                authentication.getAuthorizedClientRegistrationId(),
+                authentication.getName()
+        );
 
         String url = "https://youtube.googleapis.com/youtube/v3/videos?myRating=like&part=snippet, contentDetails, statistics";
 
-        YtDtoWrapper result = webClient
+        YtDtoWrapper ytDtoWrapper = webClient
                 .get()
                 .uri(url)
                 .header("Accept","application/json")
@@ -43,6 +48,7 @@ public class HomeController {
                 })
                 .block();
 
+        model.addAttribute("ytWrapper", ytDtoWrapper);
         return "yt";
     }
 }
