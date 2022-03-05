36aaf98c4b54e6b5a5c0cf94d06142fbedff7b46
Dariusz Szczepański <thedariusz@gmail.com>
2022-03-05 12:27:35 +0100

    added userinfo to the yt page

--- src/main/java/com/thedariusz/ytapp/controllers/HomeController.java
+++ src/main/java/com/thedariusz/ytapp/controllers/HomeController.java
@@ -3,6 +3,8 @@ package com.thedariusz.ytapp.controllers;
 import com.thedariusz.ytapp.model.UserDtoWrapper;
 import com.thedariusz.ytapp.model.YtDtoWrapper;
 import com.thedariusz.ytapp.network.YoutubeWebClient;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,7 @@ public class HomeController {
 
     private static final String EMPTY_PAGE_TOKEN = "";
     private final YoutubeWebClient youtubeWebClient;
+
 
     public HomeController(YoutubeWebClient youtubeWebClient) {
         this.youtubeWebClient = youtubeWebClient;
@@ -26,17 +29,23 @@ public class HomeController {
     @GetMapping(value = {"/home/yt/{pageToken}"})
     public String displaySecuredPageWithToken(Model model, @PathVariable String pageToken) {
         YtDtoWrapper ytDtoWrapper = youtubeWebClient.fetchYtVideos(pageToken);
+        UserDtoWrapper userDtoWrapper = youtubeWebClient.fetchUserInfo();
+        model.addAttribute("userInfo", userDtoWrapper);
         model.addAttribute("ytWrapper", ytDtoWrapper);
         return "yt";
     }
 
     @GetMapping(value = {"/home/yt"})
-    public String displaySecuredPageWithToken(Model model) {
+    public String displaySecuredPageWithToken(Model model, @AuthenticationPrincipal OidcUser principal) {
+        String userEmail = principal.getEmail();
         YtDtoWrapper ytDtoWrapper = youtubeWebClient.fetchYtVideos(EMPTY_PAGE_TOKEN);
         UserDtoWrapper userDtoWrapper = youtubeWebClient.fetchUserInfo();
         model.addAttribute("ytWrapper", ytDtoWrapper);
+        model.addAttribute("userInfo", userDtoWrapper);
         return "yt";
     }
+
+
 
 
 }
